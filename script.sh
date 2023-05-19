30a01bf8f23b4ef8bf514029ff3238025e58226f
tobiaskieser <tobias.m.kieser@gmail.com>
2023-05-19 21:25:20 +0200

    Development ongoing

--- script.sh
+++ script.sh
@@ -1,15 +1,52 @@
 #!/bin/bash
 
-url="https://cpcli.cf.eu10.hana.ondemand.com"
+# constants
+# API endpoint
+URL="https://cpcli.cf.eu10.hana.ondemand.com"
+# colors
+BLUE="\033[1;34m"
+NOCOLOR="\033[0m"
 
-startScreen () {
-    BLUE="\033[1;34m"
-    NOCOLOR="\033[0m"
-    echo ""
-    echo "================================================================="
-    echo -e "${BLUE} SAP Technology Platform - Command Line Interface Administration${NOCOLOR}"
-    echo "================================================================="
+# global Variable
+runNumber=0
+
+# Writing a = sign multiple times in a row
+equalSign() {
+    local lengthEqualSign=runNumber
+    for (( i=0; i<lengthEqualSign; i++));
+    do
+        # echo without newline
+        echo -n "="
+    done
     echo ""
 }
 
-startScreen
+startScreen () {
+    echo ""
+    # Reading a file
+    startDescription=`cat .data/greetingWindow.txt`
+    # number of the equal signs to print out
+    runNumber=32
+    equalSign
+    # interpretation of escaped characters
+    echo -e "${BLUE}$startDescription${NOCOLOR}"
+    equalSign
+    echo ""
+}
+
+login() {
+    read -p "[1] Username: " username
+    read -s -p "[2] Password: " password
+}
+
+checkLogin() {
+    echo
+    login
+}
+
+main() {
+    startScreen
+    checkLogin
+}
+
+main
